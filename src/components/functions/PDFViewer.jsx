f242a2c4a13581bb1a9e2b2d2c3c8838ef18619b
pitsy <rainer.pits@gmail.com>
2023-03-13 19:33:55 +0200

    1.1.12
    different fix for pdf viewing error

--- src/components/functions/PDFViewer.jsx
+++ src/components/functions/PDFViewer.jsx
@@ -1,7 +1,9 @@
 import '../../css/payment-method.css';
 import { Document, Page, pdfjs } from "react-pdf/dist/esm/entry.webpack5";
 import close from '../icons/x.png';
-pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
+
+const url = `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
+pdfjs.GlobalWorkerOptions.workerSrc = url
 
 export default function PDFViewer({invoicePDF, isOpen}) {
 
