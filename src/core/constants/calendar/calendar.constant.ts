e6a1bf60911b065303b067ecf81b75f61628222d
Viacheslav Shatan <sha02viacheslav@gmail.com>
2023-04-28 12:35:27 +0300

    fix timeslot

--- src/core/constants/calendar/calendar.constant.ts
+++ src/core/constants/calendar/calendar.constant.ts
@@ -2,3 +2,4 @@ export const CALENDAR_DAYS = 3
 export const CALENDAR_TIME_INTERVAL = 4
 export const CALENDAR_TIME_SLOTS = 3
 export const CALENDAR_START_TIME = 8
+export const SLOT_LABELS = ['Morning', 'Afternoon', 'Evening']
