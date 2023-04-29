8e0e7a575d77244512e75ddcfa185e15869237ab
Viacheslav Shatan <sha02viacheslav@gmail.com>
2023-04-29 09:26:55 +0300

    fix format slot id

--- src/core/utils/format-slot-id/format-slot-id.util.ts
+++ src/core/utils/format-slot-id/format-slot-id.util.ts
@@ -1,11 +1,10 @@
 import moment from 'moment/moment'
+import { CALENDAR_START_TIME, CALENDAR_TIME_INTERVAL } from '@glass/constants'
 
 export const formatSlotId = (bookingStartDate: string | Date | undefined): string => {
   // We can book every 2 hours from 08:00 AM
-  const BOOKING_START = 8
-  const BOOKING_PERIOD = 2
   if (!bookingStartDate) return ''
   return moment(bookingStartDate)
     .format('MMMDD')
-    .concat(((moment(bookingStartDate).toDate().getHours() - BOOKING_START) / BOOKING_PERIOD).toString())
+    .concat(((moment(bookingStartDate).toDate().getHours() - CALENDAR_START_TIME) / CALENDAR_TIME_INTERVAL).toString())
 }
