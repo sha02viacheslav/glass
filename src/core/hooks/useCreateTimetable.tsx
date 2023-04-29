c3281275fb77c31ff55833814e728ebed3215467
Viacheslav Shatan <sha02viacheslav@gmail.com>
2023-04-29 09:22:41 +0300

    fix calendar

--- src/core/hooks/useCreateTimetable.tsx
+++ src/core/hooks/useCreateTimetable.tsx
@@ -5,13 +5,13 @@ import { BookingOccupy, OrderState } from '@glass/enums'
 import { BookingDate, TimeRow } from '@glass/models'
 import { getCalendarService } from '@glass/services/apis/get-calendar.service'
 
-export const useCreateTimetable = (timetableToClient: (value: string[][]) => void) => {
+export const useCreateTimetable = (timetableToClient: (value: TimeRow[]) => void) => {
   const createTimetable = () => {
     // initialize with today's entry
     const newTimetable: TimeRow[] = [
       {
         date: moment().format('YYYY-MM-DD'),
-        schedules: Array(CALENDAR_TIME_SLOTS).fill(0),
+        schedules: Array(CALENDAR_TIME_SLOTS).fill(BookingOccupy.EMPTY),
       },
     ]
 
@@ -21,7 +21,7 @@ export const useCreateTimetable = (timetableToClient: (value: string[][]) => voi
       tomorrow.setDate(tomorrow.getDate() + 1)
       newTimetable.push({
         date: moment(tomorrow).format('YYYY-MM-DD'),
-        schedules: Array(CALENDAR_TIME_SLOTS).fill(0),
+        schedules: Array(CALENDAR_TIME_SLOTS).fill(BookingOccupy.EMPTY),
       })
     }
     retrieveBookings(tomorrow, newTimetable)
@@ -49,30 +49,16 @@ export const useCreateTimetable = (timetableToClient: (value: string[][]) => voi
             if (k != 0 && bookingTime < CALENDAR_START_TIME + k * CALENDAR_TIME_INTERVAL) continue
             // - if status = won -> it's Fully booked
             // - if status = open/confirm -> it's Half booked
-            row.schedules[k] = Math.max(booking.order_state === OrderState.WON ? 2 : 1, row.schedules[k])
+            const occupy = booking.order_state === OrderState.WON ? BookingOccupy.FULL : BookingOccupy.HALF
+            if (occupy == row.schedules[k]) continue
+            if (row.schedules[k] == BookingOccupy.FULL) continue
+            row.schedules[k] = occupy
           }
         }
       })
     })
 
-    // set timeslot status based on number of slots filled: 0 -> empty, 1-2 -> half, 3+ -> full
-    const newTimeTable: string[][] = []
-    for (let i = 0; i < times.length; i++) {
-      const row = times[i]
-      const data: string[] = [moment(row.date).format('MMM'), moment(row.date).format('DD')]
-      for (let j = 0; j < row.schedules.length; j++) {
-        const element = row.schedules[j]
-        if (element === 0) {
-          data.push(BookingOccupy.EMPTY)
-        } else if (element == 1) {
-          data.push(BookingOccupy.HALF)
-        } else if (element >= 2) {
-          data.push(BookingOccupy.FULL)
-        }
-      }
-      newTimeTable.push(data)
-    }
-    timetableToClient(newTimeTable)
+    timetableToClient(times)
   }
 
   useEffect(() => {
