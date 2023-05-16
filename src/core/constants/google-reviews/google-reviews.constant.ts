11a4b8879bd6fba16e554820596d7acc667bd423
Viacheslav Shatan <sha02viacheslav@gmail.com>
2023-05-16 13:41:49 +0300

    Add google reviews

---
+++ src/core/constants/google-reviews/google-reviews.constant.ts
@@ -0,0 +1,118 @@
+import { Review } from '@glass/models'
+
+export const GOOGLE_REVIEWS: Review[] = [
+  {
+    author_name: 'Виталик Харченко',
+    author_url: 'https://www.google.com/maps/contrib/106454011176778860885/reviews',
+    language: 'en',
+    original_language: 'en',
+    profile_photo_url:
+      'https://lh3.googleusercontent.com/a-/ACB-R5R50c91nDhTn9lYXJvoffjnZm-hhDkRsZXCos5z=s128-c0x00000000-cc-rp-mo',
+    rating: 5,
+    text: 'the master knows his business exactly, works quickly, accurately and is very polite in communication.  The best service I have seen, I changed the meeting time several times, but I never heard a refusal.  Agreed to get to work even at 21:00.  Recommend!',
+    time: 1674289358,
+    translated: false,
+  },
+  {
+    author_name: 'Joe Chapman',
+    author_url: 'https://www.google.com/maps/contrib/116326856987565483187/reviews',
+    language: 'en',
+    original_language: 'en',
+    profile_photo_url:
+      'https://lh3.googleusercontent.com/a-/ACB-R5Tz9B2JPpXKqazIudnF66WBE5JGJ_81VQCu0u-oYQ=s128-c0x00000000-cc-rp-mo',
+    rating: 5,
+    text: 'Absolutely great service, called up several garages and some gave me quotes almost 2x as much. He came same day of the busy Christmas period and did the job really quick. Really professional service and nice guy.',
+    time: 1671477809,
+    translated: false,
+  },
+  {
+    author_name: 'Helen Thompson',
+    author_url: 'https://www.google.com/maps/contrib/104073145157820032147/reviews',
+    language: 'en',
+    original_language: 'en',
+    profile_photo_url:
+      'https://lh3.googleusercontent.com/a/AGNmyxaDBz36sd25GW9KiVGorX5K5-H16UYTfeMfd8tT=s128-c0x00000000-cc-rp-mo',
+    rating: 5,
+    text: "I cannot recommend this company enough. Arvin kept me informed of when he was coming. He worked in the dark and didn't finish till gone 10.15. He has done a superb job and I think you would struggle to find a cheaper price. I will definitely recommend to family and friends and use you if ever I need a window on my car replacing again. So thank you",
+    time: 1682028088,
+    translated: false,
+  },
+  {
+    author_name: 'Wayne Steele',
+    author_url: 'https://www.google.com/maps/contrib/115825047159161718500/reviews',
+    language: 'en',
+    original_language: 'en',
+    profile_photo_url:
+      'https://lh3.googleusercontent.com/a/AGNmyxaih97bG04hVBhVnQb2LspNbaWrSmBOtSX3eyZ3=s128-c0x00000000-cc-rp-mo',
+    rating: 5,
+    text: 'Had an accident around 23:00 pm the previous night and was completed the following day excellent service and friendly staff as well as being really reliable would highly recommend',
+    time: 1665647501,
+    translated: false,
+  },
+  {
+    author_name: 'Laura hastings',
+    author_url: 'https://www.google.com/maps/contrib/114650644105140232914/reviews',
+    language: 'en',
+    original_language: 'en',
+    profile_photo_url:
+      'https://lh3.googleusercontent.com/a/AGNmyxbMWv-MhSBgvkAshaA_YSjQ_AQxNRuqbcJGBddJ=s128-c0x00000000-cc-rp-mo',
+    rating: 5,
+    text: 'Arvin was amazing this evening he was totally up against all weather but he never gave up. He made a stressful situation a lot easier. He was professional & polite. Thanks again so much will highly recommend.',
+    time: 1679523257,
+    translated: false,
+  },
+  {
+    author_name: 'Mitchel White',
+    author_url: 'https://www.google.com/maps/contrib/105236752064583223412/reviews',
+    language: 'en',
+    original_language: 'en',
+    profile_photo_url:
+      'https://lh3.googleusercontent.com/a/AGNmyxYuvPSN9vRe3VOADAkvuPSr-F68bCJtuCoCCcz2=s128-c0x00000000-cc-rp-mo',
+    rating: 5,
+    text: 'Great service, I called and was able to get an appointment the next day to replace my windscreen. Arvin was friendly and the windscreen looks great! Thank you so much.',
+    time: 1675532021,
+    translated: false,
+  },
+  {
+    author_name: 'Shaunna Murphy',
+    author_url: 'https://www.google.com/maps/contrib/114880543995725284944/reviews',
+    language: 'en',
+    original_language: 'en',
+    profile_photo_url:
+      'https://lh3.googleusercontent.com/a/AGNmyxZQgASHxvF1mOF31uGtUQxhAXUzjBbPON4PyFoA=s128-c0x00000000-cc-rp-mo',
+    rating: 5,
+    text: "Unfortunately got to my car after parking in a car park to see that the window had smashed. Was quite far from home and my insurance wasn't helping at all. I rang the company, they didn't have a window replacement in stock but they were able to temporarily fix me and send me on my way. From first phone call to driving home was under an hour, so impressed and can't thank you enough!",
+    time: 1680294303,
+    translated: false,
+  },
+  {
+    author_name: 'geoff gibson',
+    author_url: 'https://www.google.com/maps/contrib/103215002502545686178/reviews',
+    language: 'en',
+    original_language: 'en',
+    profile_photo_url:
+      'https://lh3.googleusercontent.com/a-/ACB-R5SKLqonCVfuUCd6ITwDUSkXGWRymjDFXFoomFU1=s128-c0x00000000-cc-rp-mo',
+    rating: 5,
+    text: 'Alvin arrived 19.45 carried out replacement in the rain like nothing was too much trouble,the guys work ethic is amazing if there is a job to do he will do it cannot recommend him highly enough and this was a same day replacement.thanks again arvin',
+    time: 1677767938,
+    translated: false,
+  },
+  {
+    author_name: 'Amy Macaulay',
+    author_url: 'https://www.google.com/maps/contrib/112817683130230766863/reviews',
+    language: 'en',
+    original_language: 'en',
+    profile_photo_url:
+      'https://lh3.googleusercontent.com/a/AGNmyxbvy7jrppbg3IOy1BZlcX8-XO8cUxYSuPKy-QDU=s128-c0x00000000-cc-rp-mo',
+    rating: 5,
+    text:
+      'Fantastic service\n' +
+      'All done through tx as-well so straight forward\n' +
+      'Booked Wednesday came to my house thursday and fitted it\n' +
+      'kept me updated on their arrival time\n' +
+      'Split monthly pay too\n' +
+      'So fast and easy 100% recommend these guys',
+    time: 1668020836,
+    translated: false,
+  },
+]
