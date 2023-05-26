1b143623b362b7da671a2100162d534d97f613ae
Viacheslav Shatan <sha02viacheslav@gmail.com>
2023-05-26 14:54:50 +0300

    fix format address

--- src/core/utils/format-address/format-address.util.ts
+++ src/core/utils/format-address/format-address.util.ts
@@ -2,7 +2,9 @@ import { Address } from '@glass/models'
 
 export const formatAddress = (address: Address | undefined, withPostCode = true): string => {
   if (!address) return ''
-  let result = `${address.line_1 || address.line_2 || '-'}, ${address.town_or_city || '-'}, ${address.county || '-'}`
-  if (withPostCode) result += `, ${address.postcode || '-'}`
+  let result = `${address.line_1 || address.line_2 || 'N/A'}, ${address.town_or_city || 'N/A'}, ${
+    address.county || 'N/A'
+  }`
+  if (withPostCode) result += `, ${address.postcode || 'N/A'}`
   return result
 }
