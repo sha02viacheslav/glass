e914ba94335f32d932cbe00f35c6e4075674bf9c
Viacheslav Shatan <sha02viacheslav@gmail.com>
2023-07-14 12:07:35 +0300

    implement time_slot_visual in calendar

--- src/core/hooks/useCreateTimetable.tsx
+++ src/core/hooks/useCreateTimetable.tsx
@@ -4,6 +4,7 @@ import { CALENDAR_LENGTH, CALENDAR_START_TIME, CALENDAR_TIME_INTERVAL, CALENDAR_
 import { BookingOccupy, OrderState } from '@glass/enums'
 import { BookingDate, TimeRow } from '@glass/models'
 import { getCalendarService } from '@glass/services/apis/get-calendar.service'
+import { slot2Hours } from '@glass/utils/slot-to-hours/slot-to-hours.util'
 
 export const useCreateTimetable = (timetableToClient: (value: TimeRow[]) => void) => {
   const createTimetable = () => {
@@ -33,7 +34,7 @@ export const useCreateTimetable = (timetableToClient: (value: TimeRow[]) => void
       if (res.success && res.data?.length) {
         fillTimeslots(
           timetable,
-          res.data.filter((item) => item.time_slot),
+          res.data.filter((item) => item.time_slot_visual),
         )
       }
     })
@@ -43,20 +44,23 @@ export const useCreateTimetable = (timetableToClient: (value: TimeRow[]) => void
     // find indexes of booked slots
     times.forEach((row) => {
       bookingData.forEach((booking) => {
-        const bookingTime = Number(booking.time_slot.split('_')[0])
-        if (moment(row.date).format('YYYY-MM-DD') === moment(booking.booking_date).format('YYYY-MM-DD')) {
-          // find what time the booking is and mark
-          for (let k = 0; k < CALENDAR_TIME_SLOTS; k++) {
-            if (k != CALENDAR_TIME_SLOTS - 1 && bookingTime >= CALENDAR_START_TIME + (k + 1) * CALENDAR_TIME_INTERVAL)
-              continue
-            if (k != 0 && bookingTime < CALENDAR_START_TIME + k * CALENDAR_TIME_INTERVAL) continue
-            // - if status = won -> it's Fully booked
-            // - if status = open/confirm -> it's Half booked
-            const occupy = booking.order_state === OrderState.WON ? BookingOccupy.FULL : BookingOccupy.HALF
-            if (occupy == row.schedules[k]) continue
-            if (row.schedules[k] == BookingOccupy.FULL) continue
-            row.schedules[k] = occupy
-          }
+        const startHour = slot2Hours(booking.time_slot_visual.split('_')[0])
+        const endHour = slot2Hours(booking.time_slot_visual.split('_')[1])
+
+        if (!startHour || !endHour) return
+        if (moment(row.date).format('YYYY-MM-DD') !== moment(booking.booking_date).format('YYYY-MM-DD')) return
+
+        // find what time the booking is and mark
+        for (let k = 0; k < CALENDAR_TIME_SLOTS; k++) {
+          if (k != CALENDAR_TIME_SLOTS - 1 && startHour >= CALENDAR_START_TIME + (k + 1) * CALENDAR_TIME_INTERVAL)
+            continue
+          if (k != 0 && endHour < CALENDAR_START_TIME + k * CALENDAR_TIME_INTERVAL) continue
+          // - if status = won -> it's Fully booked
+          // - if status = open/confirm -> it's Half booked
+          const occupy = booking.order_state === OrderState.WON ? BookingOccupy.FULL : BookingOccupy.HALF
+          if (occupy == row.schedules[k]) continue
+          if (row.schedules[k] == BookingOccupy.FULL) continue
+          row.schedules[k] = occupy
         }
       })
     })
