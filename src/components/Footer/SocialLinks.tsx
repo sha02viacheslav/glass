ad6ca8eb7b2356a321d526ad7c51f0418c4e9790
Viacheslav Shatan <sha02viacheslav@gmail.com>
2023-12-15 17:25:43 -0500

    stalk

--- src/components/Footer/SocialLinks.tsx
+++ src/components/Footer/SocialLinks.tsx
@@ -3,27 +3,30 @@ import React from 'react'
 
 export const SocialLinks: React.FC = () => {
   return (
-    <div className='d-flex gap-3 gap-md-4'>
-      <a href='https://twitter.com/fix_glass_' target='_blank' rel='noreferrer' className='social-link'>
-        <i className='fa-brands fa-x-twitter'></i>
-      </a>
-      <a
-        href='https://www.facebook.com/profile.php?id=100065230862925'
-        target='_blank'
-        rel='noreferrer'
-        className='social-link'
-      >
-        <i className='fa-brands fa-facebook-f'></i>
-      </a>
-      <a href='https://www.instagram.com/fixglassinsta/' target='_blank' rel='noreferrer' className='social-link'>
-        <i className='fa-brands fa-instagram'></i>
-      </a>
-      <a href='https://www.youtube.com/@fixglass_' target='_blank' rel='noreferrer' className='social-link'>
-        <i className='fa-brands fa-youtube'></i>
-      </a>
-      <a href='https://www.tiktok.com/@fix.glass?lang=en' target='_blank' rel='noreferrer' className='social-link'>
-        <i className='fa-brands fa-tiktok'></i>
-      </a>
+    <div>
+      <div className='text-white fnt-18 fnt-md-20 mb-2'>Stalk us</div>
+      <div className='d-flex gap-3 gap-md-4'>
+        <a href='https://twitter.com/fix_glass_' target='_blank' rel='noreferrer' className='social-link'>
+          <i className='fa-brands fa-x-twitter'></i>
+        </a>
+        <a
+          href='https://www.facebook.com/profile.php?id=100065230862925'
+          target='_blank'
+          rel='noreferrer'
+          className='social-link'
+        >
+          <i className='fa-brands fa-facebook-f'></i>
+        </a>
+        <a href='https://www.instagram.com/fixglassinsta/' target='_blank' rel='noreferrer' className='social-link'>
+          <i className='fa-brands fa-instagram'></i>
+        </a>
+        <a href='https://www.youtube.com/@fixglass_' target='_blank' rel='noreferrer' className='social-link'>
+          <i className='fa-brands fa-youtube'></i>
+        </a>
+        <a href='https://www.tiktok.com/@fix.glass?lang=en' target='_blank' rel='noreferrer' className='social-link'>
+          <i className='fa-brands fa-tiktok'></i>
+        </a>
+      </div>
     </div>
   )
 }
