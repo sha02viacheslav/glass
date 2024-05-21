1b932f9f369721b3b4ea7192e6046bf1f9c4904c
Shatan, Viacheslav <sha02viacheslav@gmail.com>
2024-05-21 09:56:49 +0300

    Partners

--- src/components/Partners/Partners.tsx
+++ src/components/Partners/Partners.tsx
@@ -1,29 +1,118 @@
-import './Partners.css'
 import React from 'react'
+import { Box, CardMedia, Typography } from '@mui/material'
 
 export const Partners: React.FC = () => {
   return (
-    <section className='sec-partners'>
-      <div className='title'>
-        We collaborate with <span className='bold'>industry-leading partners.</span>
-      </div>
-      <div className='description'>Best Glue makers in the world</div>
-      <div className='d-flex align-items-center justify-content-center gap-4'>
-        <img src={process.env.PUBLIC_URL + '/images/partner1.png'} className='img-fluid' alt='' />
-        <img src={process.env.PUBLIC_URL + '/images/partner2.png'} className='img-fluid' alt='' />
-      </div>
-      <div className='premier-partners'>Premier Partners: Leaders in Glass & Product Manufacturing</div>
-      <div className='premier-partners-wrap'>
-        <img src={process.env.PUBLIC_URL + '/images/premier-partner1.png'} alt='' />
-        <img src={process.env.PUBLIC_URL + '/images/premier-partner2.png'} alt='' />
-        <img src={process.env.PUBLIC_URL + '/images/premier-partner3.png'} alt='' />
-        <img src={process.env.PUBLIC_URL + '/images/premier-partner4.png'} alt='' />
-        <img src={process.env.PUBLIC_URL + '/images/premier-partner5.png'} alt='' />
-        <img src={process.env.PUBLIC_URL + '/images/premier-partner6.png'} alt='' />
-        <img src={process.env.PUBLIC_URL + '/images/premier-partner7.png'} alt='' />
-        <img src={process.env.PUBLIC_URL + '/images/premier-partner8.png'} alt='' />
-        <img src={process.env.PUBLIC_URL + '/images/premier-partner9.png'} alt='' />
-      </div>
-    </section>
+    <Box sx={{ background: '#f7f5ff', py: { xs: 6, lg: 16 } }}>
+      <Box className='container'>
+        <Typography
+          sx={{
+            color: 'var(--Gray-800, #14151f)',
+            textAlign: 'center',
+            fontSize: { xs: 24, lg: 36 },
+            fontStyle: 'normal',
+            fontWeight: 600,
+            lineHeight: '140%',
+          }}
+        >
+          We collaborate with <strong>industry-leading partners.</strong>
+        </Typography>
+        <Typography
+          sx={{
+            textAlign: 'center',
+            fontSize: { xs: 12, lg: 24 },
+            fontWeight: 600,
+            lineHeight: '160%',
+            letterSpacing: '0.96px',
+            textTransform: 'uppercase',
+            mt: { xs: 8, lg: 16 },
+            mb: { xs: 3, lg: 8 },
+          }}
+        >
+          Best Glue makers in the world
+        </Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 6 }}>
+          <CardMedia
+            component='img'
+            sx={{ width: 'auto', height: { xs: 80, lg: 194 } }}
+            image={process.env.PUBLIC_URL + '/images/partner1.png'}
+          />
+          <CardMedia
+            component='img'
+            sx={{ width: 'auto', height: { xs: 80, lg: 194 } }}
+            image={process.env.PUBLIC_URL + '/images/partner2.png'}
+          />
+        </Box>
+        <Typography
+          sx={{
+            textAlign: 'center',
+            fontSize: { xs: 12, lg: 24 },
+            fontWeight: 600,
+            lineHeight: '160%',
+            letterSpacing: '0.96px',
+            textTransform: 'uppercase',
+            mt: { xs: 12, lg: 30 },
+          }}
+        >
+          Premier Partners: Leaders in Glass & Product Manufacturing
+        </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            overflowX: 'scroll',
+            paddingLeft: '16px',
+            width: '100%',
+            mt: { xs: 3, lg: 8 },
+          }}
+        >
+          <CardMedia
+            component='img'
+            sx={{ width: 'auto', height: { xs: 78, lg: 194 } }}
+            image={process.env.PUBLIC_URL + '/images/premier-partner1.png'}
+          />
+          <CardMedia
+            component='img'
+            sx={{ width: 'auto', height: { xs: 78, lg: 194 } }}
+            image={process.env.PUBLIC_URL + '/images/premier-partner2.png'}
+          />
+          <CardMedia
+            component='img'
+            sx={{ width: 'auto', height: { xs: 78, lg: 194 } }}
+            image={process.env.PUBLIC_URL + '/images/premier-partner3.png'}
+          />
+          <CardMedia
+            component='img'
+            sx={{ width: 'auto', height: { xs: 78, lg: 194 } }}
+            image={process.env.PUBLIC_URL + '/images/premier-partner4.png'}
+          />
+          <CardMedia
+            component='img'
+            sx={{ width: 'auto', height: { xs: 78, lg: 194 } }}
+            image={process.env.PUBLIC_URL + '/images/premier-partner5.png'}
+          />
+          <CardMedia
+            component='img'
+            sx={{ width: 'auto', height: { xs: 78, lg: 194 } }}
+            image={process.env.PUBLIC_URL + '/images/premier-partner6.png'}
+          />
+          <CardMedia
+            component='img'
+            sx={{ width: 'auto', height: { xs: 78, lg: 194 } }}
+            image={process.env.PUBLIC_URL + '/images/premier-partner7.png'}
+          />
+          <CardMedia
+            component='img'
+            sx={{ width: 'auto', height: { xs: 78, lg: 194 } }}
+            image={process.env.PUBLIC_URL + '/images/premier-partner8.png'}
+          />
+          <CardMedia
+            component='img'
+            sx={{ width: 'auto', height: { xs: 78, lg: 194 } }}
+            image={process.env.PUBLIC_URL + '/images/premier-partner9.png'}
+          />
+        </Box>
+      </Box>
+    </Box>
   )
 }
