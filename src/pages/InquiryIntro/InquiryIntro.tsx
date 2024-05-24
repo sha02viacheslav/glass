f2cf930fe1f889f1d03697ca59cf69c03fbb39dc
Shatan, Viacheslav <sha02viacheslav@gmail.com>
2024-05-24 17:14:14 +0300

    InquiryIntro

--- src/pages/InquiryIntro/InquiryIntro.tsx
+++ src/pages/InquiryIntro/InquiryIntro.tsx
@@ -1,5 +1,5 @@
-import './InquiryIntro.css'
 import React, { useEffect, useState } from 'react'
+import { Box, CardMedia, Typography } from '@mui/material'
 import { trackPromise } from 'react-promise-tracker'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { OurMethod } from '@glass/components/OurMethod'
@@ -40,66 +40,159 @@ export const InquiryIntro: React.FC = () => {
   }, [licenseNum])
 
   return (
-    <div className='inquiry-intro-page'>
-      <div className='about-your-car'>
-        <img
-          src={
+    <Box className='inquiry-intro-page'>
+      <Box sx={{ py: { xs: 16, lg: 32 } }}></Box>
+
+      <Box
+        sx={{
+          padding: '0 16px',
+          margin: 'auto',
+          maxWidth: { xs: 300, lg: 809 },
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+        className='about-your-car'
+      >
+        <CardMedia
+          component='img'
+          sx={{ width: 'auto', height: 'auto', maxWidth: { xs: 270 } }}
+          image={
             inquiry?.step_1?.vehicle_logo_url ||
             process.env.PUBLIC_URL + '/images/mechanics-checking-planning-workshop.png'
           }
         />
-        <div className='title'>
+        <Typography
+          sx={{
+            textAlign: 'center',
+            fontSize: { xs: 20, lg: 48 },
+            fontWeight: '700',
+            lineHeight: '140%',
+            marginTop: { xs: 6, lg: 12 },
+          }}
+        >
           We need to ask few details about your {inquiry?.Make + ' ' + inquiry?.Model || 'car'}
-        </div>
-        <div className='description'>It&apos;ll take less than 5 minutes of your time</div>
-      </div>
+        </Typography>
+        <Typography
+          sx={{
+            color: 'var(--Gray-600, #6a6b71)',
+            textAlign: 'center',
+            fontSize: { xs: 16, lg: 30 },
+            fontWeight: '400',
+            lineHeight: '140%',
+            marginTop: { xs: 3, lg: 6 },
+          }}
+        >
+          It&apos;ll take less than 5 minutes of your time
+        </Typography>
+      </Box>
 
-      <div className='padding-64'></div>
+      <Box sx={{ py: 8 }}></Box>
 
-      <div className='lets-start'>
-        <button className='btn-raised w-100' onClick={() => directToCustomer()}>
-          Let&apos;s start
-        </button>
-        <div className='process-link'>
-          Find out more about our process <Link to='/process'>here</Link>
-        </div>
-      </div>
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} className='container'>
+        <Box sx={{ width: { xs: '100%', lg: 400 } }} className='lets-start'>
+          <button className='btn-raised w-100' onClick={() => directToCustomer()}>
+            Let&apos;s start
+          </button>
+          <Box
+            sx={{
+              color: 'var(--Gray-700, #474747)',
+              fontSize: { xs: 16, lg: 20 },
+              lineHeight: '130%',
+              marginTop: 3,
+              textAlign: 'center',
+              a: {
+                color: 'var(--Light-Blue---Primary-500, #225fc2)',
+                fontWeight: '600',
+                textDecoration: 'none',
+              },
+            }}
+            className='process-link'
+          >
+            Find out more about our process <Link to='/process'>here</Link>
+          </Box>
+        </Box>
+      </Box>
 
-      <div className='padding-64'></div>
+      <Box sx={{ py: { xs: 8, lg: 16 } }}></Box>
 
-      <div className='know-more-service'>
-        <div className='title'>Want to know more about our service?</div>
-        <div className='description'>We Guarantee a seamless install, making your windscreen look like new again</div>
-      </div>
+      <Box className='container'>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Typography
+            sx={{
+              textAlign: 'center',
+              fontSize: { xs: 30, lg: 36 },
+              fontWeight: '700',
+              lineHeight: '120%',
+              letterSpacing: '-0.3px',
+            }}
+          >
+            Want to know more about our service?
+          </Typography>
+          <Typography
+            sx={{
+              color: 'var(--Gray-700, #474747)',
+              textAlign: 'center',
+              fontSize: { xs: 16, lg: 20 },
+              lineHeight: '170%',
+              marginTop: 6,
+              maxWidth: 490,
+            }}
+          >
+            We Guarantee a seamless install, making your windscreen look like new again
+          </Typography>
+        </Box>
+      </Box>
 
-      <div className='padding-64'></div>
+      <Box sx={{ py: { xs: 8, lg: 12 } }}></Box>
 
       <OurMethod beforeAfterType={BeforeAfterType.ALL} showVideos={false} />
 
-      <div className='padding-64'></div>
+      <Box sx={{ py: { xs: 8, lg: 16 } }}></Box>
 
-      <div className='after-installation'>
-        <div className='title'>Drive Away Safely in 60 minutes After Installation!</div>
-        <div className='description'>
-          We use premium crash-tested SikaTack® DRIVE glue, which sets quickly and is guaranteed to stay in place once
-          set
-        </div>
-      </div>
+      <Box sx={{ background: { lg: '#fff' }, py: { lg: 12 } }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} className='container'>
+          <Box sx={{ width: { xs: '100%', lg: 650 } }}>
+            <Typography
+              sx={{
+                textAlign: 'center',
+                fontSize: { xs: 24, lg: 36 },
+                fontWeight: '600',
+                lineHeight: '150%',
+              }}
+            >
+              Drive Away Safely in 60 minutes After Installation!
+            </Typography>
+            <Typography
+              sx={{
+                color: 'var(--Gray-700, #474747)',
+                textAlign: 'center',
+                fontSize: { xs: 16, lg: 20 },
+                fontWeight: '600',
+                lineHeight: '170%',
+                marginTop: 4,
+              }}
+            >
+              We use premium crash-tested SikaTack® DRIVE glue, which sets quickly and is guaranteed to stay in place
+              once set
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
 
-      <div className='padding-64'></div>
+      <Box sx={{ py: { xs: 8, lg: 16 } }}></Box>
 
       <InstallmentBenefits />
 
-      <div className='padding-64'></div>
+      <Box sx={{ py: { xs: 8, lg: 16 } }}></Box>
 
       <Partners />
 
-      <div className='padding-64'></div>
+      <Box sx={{ py: { xs: 8, lg: 16 } }}></Box>
 
       <Testimonials />
 
-      <div className='padding-64'></div>
-      <div className='padding-64'></div>
-    </div>
+      <Box sx={{ py: { xs: 16, lg: 25 } }}></Box>
+    </Box>
   )
 }
